e7ccc9ae960eee8c93870cfc5996885f4c038f18
mrbradfo <mrbradfo@gmail.com>
2018-01-22 09:13:48 -0700

    Update cube.h
    Flipped the direction of the letters

--- cube.h
+++ cube.h
@@ -44,157 +44,157 @@ public:
         switch(l){
             // Just letters I need for now
             case '!':{
-                set2DPattern(3, " #  ", c, z);
-                set2DPattern(2, " #  ", c, z);
-                set2DPattern(1, "    ", c, z);
                 set2DPattern(0, " #  ", c, z);
+                set2DPattern(1, " #  ", c, z);
+                set2DPattern(2, "    ", c, z);
+                set2DPattern(3, " #  ", c, z);
                 break;
             }
             case '1':{
-                set2DPattern(3, " #  ", c, z);
-                set2DPattern(2, "##  ", c, z);
-                set2DPattern(1, " #  ", c, z);
-                set2DPattern(0, "### ", c, z);
+                set2DPattern(0, " #  ", c, z);
+                set2DPattern(1, "##  ", c, z);
+                set2DPattern(2, " #  ", c, z);
+                set2DPattern(3, "### ", c, z);
                 break;
             }
             case '2':{
-                set2DPattern(3, "####", c, z);
-                set2DPattern(2, "  ##", c, z);
-                set2DPattern(1, "##  ", c, z);
                 set2DPattern(0, "####", c, z);
+                set2DPattern(1, "  ##", c, z);
+                set2DPattern(2, "##  ", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case '4':{
-                set2DPattern(3, "#  #", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "####", c, z);
-                set2DPattern(0, "   #", c, z);
+                set2DPattern(0, "#  #", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "####", c, z);
+                set2DPattern(3, "   #", c, z);
                 break;
             }
             case 'A':{
-                set2DPattern(3, " ## ", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "####", c, z);
-                set2DPattern(0, "#  #", c, z);
+                set2DPattern(0, " ## ", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "####", c, z);
+                set2DPattern(3, "#  #", c, z);
                 break;
             }
             case 'B':{
-                set2DPattern(3, "##  ", c, z);
-                set2DPattern(2, "# # ", c, z);
-                set2DPattern(1, "## #", c, z);
-                set2DPattern(0, "### ", c, z);
+                set2DPattern(0, "##  ", c, z);
+                set2DPattern(1, "# # ", c, z);
+                set2DPattern(2, "## #", c, z);
+                set2DPattern(3, "### ", c, z);
                 break;
             }
             case 'C':{
-                set2DPattern(3, "####", c, z);
-                set2DPattern(2, "#   ", c, z);
-                set2DPattern(1, "#   ", c, z);
                 set2DPattern(0, "####", c, z);
+                set2DPattern(1, "#   ", c, z);
+                set2DPattern(2, "#   ", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case 'D':{
-                set2DPattern(3, "### ", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "#  #", c, z);
                 set2DPattern(0, "### ", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "#  #", c, z);
+                set2DPattern(3, "### ", c, z);
                 break;
             }
             case 'E':{
-                set2DPattern(3, "####", c, z);
-                set2DPattern(2, "##  ", c, z);
-                set2DPattern(1, "#   ", c, z);
                 set2DPattern(0, "####", c, z);
+                set2DPattern(1, "##  ", c, z);
+                set2DPattern(2, "#   ", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case 'G':{
-                set2DPattern(3, "####", c, z);
-                set2DPattern(2, "#   ", c, z);
-                set2DPattern(1, "#  #", c, z);
                 set2DPattern(0, "####", c, z);
+                set2DPattern(1, "#   ", c, z);
+                set2DPattern(2, "#  #", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case 'H':{
-                set2DPattern(3, "#  #", c, z);
-                set2DPattern(2, "####", c, z);
-                set2DPattern(1, "#  #", c, z);
                 set2DPattern(0, "#  #", c, z);
+                set2DPattern(1, "####", c, z);
+                set2DPattern(2, "#  #", c, z);
+                set2DPattern(3, "#  #", c, z);
                 break;
             }
             case 'I':{
-                set2DPattern(3, "### ", c, z);
-                set2DPattern(2, " #  ", c, z);
-                set2DPattern(1, " #  ", c, z);
                 set2DPattern(0, "### ", c, z);
+                set2DPattern(1, " #  ", c, z);
+                set2DPattern(2, " #  ", c, z);
+                set2DPattern(3, "### ", c, z);
                 break;
             }
             case 'L':{
-                set2DPattern(3, "#   ", c, z);
-                set2DPattern(2, "#   ", c, z);
+                set2DPattern(0, "#   ", c, z);
                 set2DPattern(1, "#   ", c, z);
-                set2DPattern(0, "####", c, z);
+                set2DPattern(2, "#   ", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case 'N':{
-                set2DPattern(3, "#  #", c, z);
-                set2DPattern(2, "## #", c, z);
-                set2DPattern(1, "# ##", c, z);
                 set2DPattern(0, "#  #", c, z);
+                set2DPattern(1, "## #", c, z);
+                set2DPattern(2, "# ##", c, z);
+                set2DPattern(3, "#  #", c, z);
                 break;
             }
             case 'O':{
-                set2DPattern(3, " ## ", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "#  #", c, z);
                 set2DPattern(0, " ## ", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "#  #", c, z);
+                set2DPattern(3, " ## ", c, z);
                 break;
             }
             case 'P':{
-                set2DPattern(3, "### ", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "### ", c, z);
-                set2DPattern(0, "#   ", c, z);
+                set2DPattern(0, "### ", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "### ", c, z);
+                set2DPattern(3, "#   ", c, z);
                 break;
             }
             case 'R':{
-                set2DPattern(3, "### ", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "### ", c, z);
-                set2DPattern(0, "#  #", c, z);
+                set2DPattern(0, "### ", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "### ", c, z);
+                set2DPattern(3, "#  #", c, z);
                 break;
             }
             case 'S':{
-                set2DPattern(3, "####", c, z);
-                set2DPattern(2, "##  ", c, z);
-                set2DPattern(1, "  ##", c, z);
                 set2DPattern(0, "####", c, z);
+                set2DPattern(1, "##  ", c, z);
+                set2DPattern(2, "  ##", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case 'T':{
-                set2DPattern(3, "### ", c, z);
-                set2DPattern(2, " #  ", c, z);
+                set2DPattern(0, "### ", c, z);
                 set2DPattern(1, " #  ", c, z);
-                set2DPattern(0, " #  ", c, z);
+                set2DPattern(2, " #  ", c, z);
+                set2DPattern(3, " #  ", c, z);
                 break;
             }
             case 'U':{
-                set2DPattern(3, "#  #", c, z);
-                set2DPattern(2, "#  #", c, z);
+                set2DPattern(0, "#  #", c, z);
                 set2DPattern(1, "#  #", c, z);
-                set2DPattern(0, "####", c, z);
+                set2DPattern(2, "#  #", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
             case 'V':{
-                set2DPattern(3, "#  #", c, z);
-                set2DPattern(2, "#  #", c, z);
+                set2DPattern(0, "#  #", c, z);
                 set2DPattern(1, "#  #", c, z);
-                set2DPattern(0, " ## ", c, z);
+                set2DPattern(2, "#  #", c, z);
+                set2DPattern(3, " ## ", c, z);
                 break;
             }
             case 'W':{
-                set2DPattern(3, "#  #", c, z);
-                set2DPattern(2, "#  #", c, z);
-                set2DPattern(1, "# ##", c, z);
-                set2DPattern(0, "####", c, z);
+                set2DPattern(0, "#  #", c, z);
+                set2DPattern(1, "#  #", c, z);
+                set2DPattern(2, "# ##", c, z);
+                set2DPattern(3, "####", c, z);
                 break;
             }
         }
